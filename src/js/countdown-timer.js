ce6b7b61a078f3630926e71d8a0732a648e419b4
Glemakk <kovgar.brand@gmail.com>
2021-05-10 22:38:29 +0200

    Задеплоил

--- src/js/countdown-timer.js
+++ src/js/countdown-timer.js
@@ -91,9 +91,7 @@ const timer = new CountdownTimer({
 //    return String(value).padStart(2, 0)
 // };
 
-
 // function getTimeComponents(time) {
-
 // const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
 // const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
 // const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
