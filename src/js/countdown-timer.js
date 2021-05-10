3d67ef435d9a70826ecec81c9b318ddaf5936ad2
Glemakk <kovgar.brand@gmail.com>
2021-05-10 22:34:21 +0200

    finished

--- src/js/countdown-timer.js
+++ src/js/countdown-timer.js
@@ -1,19 +1,19 @@
 import { refs } from './constants'
 
-const date = new Date(2021, 4, 25, 5, 40, 0, 0);
+// const date = new Date(2021, 4, 25, 5, 40, 0, 0);
 
-// Проверим
-//console.log(`2021, 5, 25, 5, 40 соответствует ${date.getTime()}`);
-// "2017, 6, 14, 5, 40 соответствует 1500000000000"
+// // Проверим
+// //console.log(`2021, 5, 25, 5, 40 соответствует ${date.getTime()}`);
+// // "2017, 6, 14, 5, 40 соответствует 1500000000000"
 
-/* С помощью методов set */
-// Создадим новый объект с текущей датой
-const secondDate = new Date();
+// /* С помощью методов set */
+// // Создадим новый объект с текущей датой
+// const secondDate = new Date();
 
-// Изменим год и в нем же месяц и день месяца
-secondDate.setFullYear(2021, 5, 25);
-// Изменим час и в нем же минуты, секунды и милисекунды
-secondDate.setHours(5, 40, 0, 0);
+// // Изменим год и в нем же месяц и день месяца
+// secondDate.setFullYear(2021, 5, 25);
+// // Изменим час и в нем же минуты, секунды и милисекунды
+// secondDate.setHours(5, 40, 0, 0);
 
 // Проверим
 //console.log(`2017, 6, 14, 5, 40 соответствует ${secondDate.getTime()}`);
@@ -21,54 +21,91 @@ secondDate.setHours(5, 40, 0, 0);
 
 // new CountdownTimer({
 //   selector: '#timer-1',
-//   targetDate: new Date('Jul 17, 2019'),
+//   targetDate: new Date(2021, 4, 25, 5, 40, 0, 0);,
 // });
 
 
-// class CountdownTimer{
-//     constructor() {
-//         this.selector = '#timer-1';
-//         this.targetDate = new Date(2021, 4, 25, 5, 40, 0, 0);
-//     }
-// }
-
-//-----------------without class
-const timer = {
-    start() {
-        const startTime = date.getTime();
+class CountdownTimer{
+    constructor({ targetDate }) {
+        this.targetDate = targetDate;
+        this.start();
+    }
+start() {
+        const startTime = this.targetDate.getTime();
         setInterval(() => {
             const currentTime = Date.now();
             const deltaTime = startTime -currentTime;
-            const time = getTimeComponents(deltaTime);
-            updateTimerFace(time);
+            const time = this.getTimeComponents(deltaTime);
+            this.updateTimerFace(time);
             console.log(time);
         }, 1000);
-    },
-};
+    }
 
-timer.start();
-
-//Метод padStart() заполняет текущую строку другой строкой (несколько раз, если нужно)
-//так, что итоговая строка достигает заданной длины.Заполнение осуществляется в начале(слева) текущей строки.
-function pad(value) {
-   return String(value).padStart(2, 0)
-};
-
-
-function getTimeComponents(time) {
-
-const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    getTimeComponents(time) {
+const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
     
     return { days, hours, mins, secs };
-};
-
-function updateTimerFace({days, hours, mins, secs}) {
+    }
+    
+    pad(value) {
+   return String(value).padStart(2, 0)
+    }
+    
+    updateTimerFace({days, hours, mins, secs}) {
     refs.days.textContent = `${days}`;
     refs.hours.textContent = `: ${hours}`;
     refs.mins.textContent = `: ${mins}`;
     refs.secs.textContent = `: ${secs}`;
 }
+
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date(2021, 4, 25, 5, 40, 0, 0),
+});
+
+
+// //-----------------without class
+// const timer = {
+//     start() {
+//         const startTime = date.getTime();
+//         setInterval(() => {
+//             const currentTime = Date.now();
+//             const deltaTime = startTime -currentTime;
+//             const time = getTimeComponents(deltaTime);
+//             updateTimerFace(time);
+//             console.log(time);
+//         }, 1000);
+//     },
+// };
+
+// timer.start();
+
+// // Метод padStart() заполняет текущую строку другой строкой (несколько раз, если нужно)
+// // так, что итоговая строка достигает заданной длины.Заполнение осуществляется в начале(слева) текущей строки.
+// function pad(value) {
+//    return String(value).padStart(2, 0)
+// };
+
+
+// function getTimeComponents(time) {
+
+// const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+// const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+// const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+// const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+    
+//     return { days, hours, mins, secs };
+// };
+
+// function updateTimerFace({days, hours, mins, secs}) {
+//     refs.days.textContent = `${days}`;
+//     refs.hours.textContent = `: ${hours}`;
+//     refs.mins.textContent = `: ${mins}`;
+//     refs.secs.textContent = `: ${secs}`;
+// }
 
