352f9bfe866b274f0721bf76ed2de4856de068b6
Glemakk <kovgar.brand@gmail.com>
2021-05-10 21:44:16 +0200

    Finished without class

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+
+import './js/constants'
+import './js/countdown-timer'
---
+++ src/js/constants.js
@@ -0,0 +1,7 @@
+export const refs = {
+    container: document.getElementById('timer-1'),
+    days: document.querySelector('[data-value=days]'),
+    hours: document.querySelector('[data-value=hours]'),
+    mins: document.querySelector('[data-value=mins]'),
+    secs: document.querySelector('[data-value=secs]'),
+}
